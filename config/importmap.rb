15cd67da85f18e41302adef01d56fb2efccbf7a2
Pudding(Lloyd) <johnlloyddesape@gmail.com>
2024-03-21 21:35:29 +0800

    added bootswatch theme and bootstrap 5 with bootstrap icon helper

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,4 +4,6 @@ pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin "popper", to: "popper.js", preload: true
+pin "bootstrap", to: "bootstrap.min.js", preload:true
 pin_all_from "app/javascript/controllers", under: "controllers"
