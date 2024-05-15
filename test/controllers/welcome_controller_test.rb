4a1f85ad589f6cbc86e73c9f7385a3798ebe442d
Pudding(Lloyd) <johnlloyddesape@gmail.com>
2024-05-15 09:39:18 +0800

    fixed failing unit test

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -7,25 +7,25 @@ class WelcomeControllerTest < ActionDispatch::IntegrationTest
     @unregistered_user = users(:unregistered)
   end
   test 'public user should redirect to the sign in page' do
-    get welcome_path
+    get root_path
     assert_redirected_to new_user_session_path
   end
 
   test 'staff user should be able to access the welcome page' do
     sign_in @staff_user
-    get welcome_path
+    get root_path
     assert_response :success
   end
 
   test 'administrator user should be able to access the welcome page' do
     sign_in @admin_user
-    get welcome_path
+    get root_path
     assert_response :success
   end
 
   test 'unregistered user should be able to access the welcome page' do
     sign_in @unregistered_user
-    get welcome_path
+    get root_path
     assert_response :success
   end
 end
