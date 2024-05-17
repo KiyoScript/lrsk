ccd22fbe39296c485484008cc4b941f7836be681
Pudding(Lloyd) <johnlloyddesape@gmail.com>
2024-05-17 19:51:12 +0800

    Updated Validation

--- app/models/consult.rb
+++ app/models/consult.rb
@@ -1,3 +1,4 @@
 class Consult < ApplicationRecord
   belongs_to :patient
+  validates :reason, presence: true
 end
--- app/models/physical_examination.rb
+++ app/models/physical_examination.rb
@@ -1,3 +1,4 @@
 class PhysicalExamination < ApplicationRecord
   belongs_to :patient
+  validates :others, :diagnosis, :district_physician, :plan, presence: true
 end
